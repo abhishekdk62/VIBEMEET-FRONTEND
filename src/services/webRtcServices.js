4f7d4c8fe4aa53e845df4e37c1aeca120feb9793
ABHISHEK <abhidk62@gmail.com>
2025-10-09 12:32:29 +0530

    added fix

--- src/services/webRtcServices.js
+++ src/services/webRtcServices.js
@@ -23,9 +23,11 @@ class WebRTCService {
     }
 
     this.socket = io(
-      import.meta.env.VITE_NODE_ENV == "dev"
-        ? import.meta.env.VITE_API_URL_DEV
-        : import.meta.env.VITE_API_URL_PROD,
+      `${
+        import.meta.env.VITE_NODE_ENV == "dev"
+          ? import.meta.env.VITE_API_URL_DEV
+          : import.meta.env.VITE_API_URL_PROD
+      }/meetings`,
       {
         auth: { token },
         forceNew: true,
